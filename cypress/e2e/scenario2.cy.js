4b9080e6c1a65a1e7ff9a8ab94bc4b830055776a
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-23 19:09:51 -0300

    feat: creating step 3 of scenario 2

--- cypress/e2e/scenario2.cy.js
+++ cypress/e2e/scenario2.cy.js
@@ -1,11 +1,17 @@
 class RegisterForm {
     elements = {
         titleInput: () => cy.get('#title'),
+        imageUrlInput: () => cy.get('#imageUrl')
     }
 
     typeTitle(text) {
         if(!text) return;
         this.elements.titleInput().type(text)
+    }
+
+    typeImageUrl(text) {
+        if(!text) return;
+        this.elements.imageUrlInput().type(text)
     }
 }
 
@@ -18,7 +24,8 @@ describe('Image Registration', () => {
     })
 
     const input = {
-        title: 'Alien BR'
+        title: 'Alien BR',
+        imageUrl: 'https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg'
     }
 
     it('Given I am on the image registration page', () => {
@@ -27,9 +34,10 @@ describe('Image Registration', () => {
     it(`When I enter "${input.title}" in the title field`, () => {
         registerForm.typeTitle(input.title)
     })
-    it(`And I enter "https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg" in the URL field`)
-    it(`When I enter "https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg" in the URL field`)
-    it(`When I hit enter to submit the form`)
+    it(`And I enter "${input.imageUrl}" in the URL field`, () => {
+        registerForm.typeImageUrl(input.imageUrl)
+    })
+    it(`Then I hit enter to submit the form`)
     it(`Then I should see a check icon in the title field`)
     it(`Then I should see a check icon in the imageUrl field`)
     it(`And the list of registered images should be updated with the new item`)
