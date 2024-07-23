69ef854057547bd627be23a366f607a61f28e3c3
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-23 17:48:44 -0300

    feat: creating scenario 1 structure

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
+describe('Image Registration', () => {
+  describe('Submitting an image with invalid inputs', () => {
+
+    const input = {
+      title: '',
+      url: ''
+    }
+
+    it('Given I am on the image registration page', () => {
+      cy.visit('/')
+    })
+
+    it(`When I enter "${input.title}" in the title field`)
+    it(`When I enter "${input.url}" in the Url field`)
+    it(`Then I click the submit button`)
+    it(`Then I should see "Please type a title for the image" message above the title field`)
+    it(`And I should see "Please type a valid URL" message above the imageUrl field`)
+    it(`And I should see an exclamation icon in the title and URL fields`)
+    
   })
+
 })
