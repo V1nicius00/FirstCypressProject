3aca4fdafb190edf2b28963e7c8c9c2d69e99d1b
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-07-23 18:47:06 -0300

    feat: creating scenario 2 structure

---
+++ cypress/e2e/scenario2.cy.js
@@ -0,0 +1,18 @@
+describe('Image Registration', () => {
+  describe('Submitting an image with valid inputs using enter key', () => {
+    after(() => {
+      cy.clearAllLocalStorage()
+    })
+
+    it('Given I am on the image registration page')
+    it(`When I enter "Alien BR" in the title field`)
+    it(`Then I should see a check icon in the title field`)
+    it(`When I enter "https://cdn.mos.cms.futurecdn.net/eM9EvWyDxXcnQTTyH8c8p5-1200-80.jpg" in the URL field`)
+    it(`Then I should see a check icon in the imageUrl field`)
+    it(`Then I can hit enter to submit the form`)
+    it(`And the list of registered images should be updated with the new item`)
+    it(`Then The inputs should be cleared`)
+    
+  })
+
+})
